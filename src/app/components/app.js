efbd96da89e84734917109eca20bc05e4cc8445e
Donovan Hutchinson <donovanh+git@gmail.com>
2018-03-30 11:09:32 +0100

    Adding offline status message

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar'
 import WeatherList from '../containers/weather_list'
 
+const NetworkMessage = () => {
+  if (typeof window === 'undefined') return null
+  const online = window.navigator.onLine
+  let message = ''
+  if (!online) message = 'You are offline'
+  
+  return (
+    <div className="network-message">{message}</div>
+  )
+}
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <NetworkMessage />
         <SearchBar />
         <WeatherList />
       </div>
